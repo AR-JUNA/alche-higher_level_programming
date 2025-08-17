53f23b1d65b94b205accd4cefb66d3fd7731c413
AR-JUNA <a.gyan@alustudent.com>
2025-08-17 00:59:23 -0700

    Add uppercase function and 8-main.py test script

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for c in str:
-        if 97 <= ord(c) <= 122:
+def uppercase(text):
+    """Print a string in uppercase followed by a new line."""
+    for c in text:
+        if 'a' <= c <= 'z':
             print("{}".format(chr(ord(c) - 32)), end="")
         else:
             print("{}".format(c), end="")
