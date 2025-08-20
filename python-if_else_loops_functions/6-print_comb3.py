39489fc7e741b68d70734268e75c74960de39527
AR-JUNA <a.gyan@alustudent.com>
2025-08-20 00:44:26 -0700

    Add 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(0, 10):
     for j in range(i + 1, 10):
-        if i != 8 or j != 9:
-            print("{:01}{:01}".format(i, j), end=", ")
-        else:
-            print("{:01}{:01}".format(i, j))
+        print("{:d}{:d}".format(i, j), end="\n" if (i == 8 and j == 9) else ", ")
