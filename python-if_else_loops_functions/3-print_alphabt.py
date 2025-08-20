68c3eeb2ca5f78efaec8946aaf986757f41dfddd
AR-JUNA <a.gyan@alustudent.com>
2025-08-20 11:24:17 -0700

    Delete python-import_modules directory

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+<<<<<<< HEAD
 for c in range(97, 123):
     if c == 101 or c == 113:  # skip 'e' and 'q'
         continue
     print("{:c}".format(c), end="")
+=======
+for i in range(97, 123):
+    if i != 101 and i != 113:  # skip 'e' (101) and 'q' (113)
+        print("{}".format(chr(i)), end="")
+>>>>>>> Add script to print lowercase alphabet without q and e
